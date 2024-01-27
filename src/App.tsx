21b1b8a14351261d1d6fcb43d223f42b6395fdac
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2024-01-27 09:15:48 +0530

    rain water tapping problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Ten from "./components/10";
+import Eleven from "./components/11";
 import Six from "./components/6";
 import QueFive from "./components/queFive";
 import QueFour from "./components/queFour";
@@ -47,6 +48,9 @@ const App = () => {
         </li>
         <li>
           <Ten />
+        </li>
+        <li>
+          <Eleven />
         </li>
       </ol>
       <hr />
---
+++ src/components/11.tsx
@@ -0,0 +1,43 @@
+function trap(height: number[]): number {
+  if (height.length === 0) {
+    return 0;
+  }
+
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  let result = 0;
+
+  while (left < right) {
+    leftMax = Math.max(leftMax, height[left]);
+    rightMax = Math.max(rightMax, height[right]);
+
+    if (height[left] < height[right]) {
+      result += leftMax - height[left];
+      left++;
+    } else {
+      result += rightMax - height[right];
+      right--;
+    }
+  }
+
+  return result;
+}
+
+const Eleven = () => {
+  const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+  const result = trap(height);
+  const link ="https://leetcode.com/problems/trapping-rain-water/description/";
+  return (
+    <div>
+      {`${result} units of rainwater cann be tapped. `}(
+      <span style={{ color: "green" }}>
+        link of question-<a href={link}>link</a>
+      </span>
+      )
+    </div>
+  );
+};
+
+export default Eleven;
